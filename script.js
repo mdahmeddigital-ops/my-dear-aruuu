7e1dcec7c6ae3971433f4bc30c81b43986927205
mdahmeddigital-ops <mdahmeddigital@gmail.com>
2026-07-25 15:18:37 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
+function openLetter() {
 
+    document.body.innerHTML = `
+
+    <div class="letterPage">
+
+        <div class="letterBox">
+
+            <div class="smallHeart">
+                💖
+            </div>
+
+            <h1>
+                My Dear Aruuu 💖
+            </h1>
+
+
+            <p>
+
+            🫶 You are my daily happiness 🌞🤍,
+            <br><br>
+
+            my favorite notification 📱💕,
+            my emergency contact 😂❤️,
+            and my biggest flex 😌✨.
+            <br><br>
+
+            You are my peace ☁️🛋️,
+            my comfort in every moment 🥺💞,
+            and my loudest cheerleader 📣❤️.
+            <br><br>
+
+            Thank you for always believing in me 🌈🤍,
+            and making my life more beautiful every day ✨.
+            <br><br>
+
+            You are beautiful inside and out 🌸🤍,
+            kind-hearted ❤️💕,
+            and the strongest soul I have ever known 💪🥺.
+            <br><br>
+
+            Never stop smiling 😊🌻,
+            because your smile is my favorite place in this world 🌍❤️.
+            <br><br>
+
+            I love you endlessly 💋∞❤️
+
+            <br><br>
+
+            Forever & Always,
+            <br>
+            My Dear Aruuu 💖
+
+            </p>
+
+        </div>
+
+    </div>
+
+    `;
+
+}
